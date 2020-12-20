e942141bf4520144c641caa67772c63999bcaee6
morgan <facorat.morgan@gmail.com>
2020-12-20 17:41:09 +0000

    travis add

---
+++ nutella_project/settings/travis.py
@@ -0,0 +1,13 @@
+from . import *
+DATABASES = {
+             'default': {
+                  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                  'NAME': 'nutella',
+                  'USER': 'morgan',
+                  'PASSWORD': 'ty',
+                  'HOST': 'localhost',
+                  'PORT': '5432',
+                    }
+                         }
+
+~                                                                                                                       ~                                 
